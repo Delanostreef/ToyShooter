c63d9d6531fc40ad2bc9806cb157d9c6133bfb10
twinkelier <78417513+twinkelier@users.noreply.github.com>
2021-04-09 14:17:25 +0200

    art branch update

---
+++ ToyShooter/Assets/Dev/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector2 lowerBounds, upperBounds;
+    public float speed, tiltAngle;
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    
+
+        transform.position = new Vector3
+        (
+        Mathf.Clamp(transform.position.x + (input.x * speed), lowerBounds.x, upperBounds.x),
+        Mathf.Clamp(transform.position.y + (input.y * speed), lowerBounds.y, upperBounds.y),
+        0
+        );
+
+        //als je omhoog en omlaag beweegt, tilt je scheepje omlaag en omhoog
+        transform.eulerAngles = new Vector3(0, 0, tiltAngle * Input.GetAxis("Vertical"));
+    }
+}
