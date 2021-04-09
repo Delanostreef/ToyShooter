04c30a5a8dd71fce81f9feab616342e6cf101bdd
Peanutts <200695@student.glu.nl>
2021-04-09 13:30:25 +0200

    big commit no breaky

--- ToyShooter/Assets/Dev/Scripts/ObjectPooler.cs
+++ ToyShooter/Assets/Dev/Scripts/ObjectPooler.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class ObjectPooler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [System.Serializable]
+    public class Pool
     {
-        
+        public string tag;
+        public GameObject prefab;
+        public int size;
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    void Start()
     {
-        
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject bullet = Instantiate(pool.prefab);
+                bullet.SetActive(false);
+                objectPool.Enqueue(bullet);
+            }
+
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+
     }
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with tag " + tag + " doesn't excist.");
+            return null;
+        }
+
+        
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+    
+
 }
