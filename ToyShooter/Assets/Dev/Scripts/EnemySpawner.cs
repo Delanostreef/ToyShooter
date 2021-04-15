815ff1f34acedc10ee84c3ca4e0da4cc13761113
twinkelier <78417513+twinkelier@users.noreply.github.com>
2021-04-15 13:02:01 +0200

    Update EnemySpawner.cs

--- ToyShooter/Assets/Dev/Scripts/EnemySpawner.cs
+++ ToyShooter/Assets/Dev/Scripts/EnemySpawner.cs
@@ -7,45 +7,30 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private GameObject[] _enemies = null;
     [SerializeField] private List<GameObject> _enemyList;
     [SerializeField] private int _maxEnemiesSpawned;
-    [SerializeField] private int _enemiesSpawned;
-
-    private void Start()
-    {
-        //InvokeRepeating("SpawnEnemy", 3f, Random.Range(2, 5));
-    }
+    private bool _isSpawning = false;
 
     private void Update()
     {
-        if (_enemyList.Count <= _maxEnemiesSpawned - 1 && _enemiesSpawned <= _maxEnemiesSpawned)
+        if (_enemyList.Count <= _maxEnemiesSpawned && !_isSpawning)
         {
-            Invoke("SpawnEnemy", Random.Range(2, 5));
-            //InvokeRepeating("SpawnEnemy", 0f, Random.Range(2, 5));
-            //CancelInvoke("SpawnEnemy");
+            _isSpawning = true;
 
-            //StartCoroutine(SpawnEnemy(Random.Range(2, 5)));
-
-            _enemiesSpawned++;
+            StartCoroutine(SpawnEnemy(Random.Range(2, 5)));
         }
-        //else if (_enemyList.Count == _maxEnemiesSpawned)
-        //{
-        //    StopCoroutine("SpawnEnemy");
-        //}
     }
 
-    private void SpawnEnemy()
+    private IEnumerator SpawnEnemy(float timeToSpawn)
     {
+        yield return new WaitForSeconds(timeToSpawn);
+
         Vector3 randomSpawnPoint = new Vector3(8f, Random.Range(-4f, 4f), 11);
 
         GameObject enemy = Instantiate(_enemies[Random.Range(0, _enemies.Length)], randomSpawnPoint, _enemies[Random.Range(0, _enemies.Length)].transform.rotation);
 
         AddEnemy(enemy);
 
+        _isSpawning = false;
     }
-
-    //private IEnumerator SpawnEnemy(float timeToSpawn)
-    //{
-    //    yield return new WaitForSeconds(timeToSpawn);
-    //}
 
     public void AddEnemy(GameObject go)
     {
