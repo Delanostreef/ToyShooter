c9e2e6a5b34ac0faa443103910021c7cc53633da
twinkelier <78417513+twinkelier@users.noreply.github.com>
2021-04-21 13:20:34 +0200

    stuff

--- ToyShooter/Assets/Dev/Scripts/Score.cs
+++ ToyShooter/Assets/Dev/Scripts/Score.cs
@@ -7,6 +7,13 @@ public class Score : MonoBehaviour
     [Header("Score")]
     public int currentScore;
 
+    public int lastHighscore;
+
+    private void Update()
+    {
+        
+    }
+
     public void ScoreAdder(int score)
     {
         currentScore = currentScore + score;
